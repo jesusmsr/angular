45ccb391557cfc52a4d17694bbea0f00eaf0a20d
jesusmsr <jsr20xx@gmail.com>
2022-04-01 12:39:56 +0200

    Add parallax directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ParallaxDirective } from './directives/parallax.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule,
